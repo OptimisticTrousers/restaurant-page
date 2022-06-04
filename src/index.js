5754715a464840d41db36788dc5595e2ba088d98
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-03 23:09:02 -0400

    Creating modules and folders for code organization

--- src/index.js
+++ src/index.js
@@ -53,28 +53,7 @@ homeButton.addEventListener('click', () => {
 
     removePage();
     changePageTitle("Welcome to Optimistic's Fried Chicken!");
-
-
-    pageContent.classList.add('div#content');
-
-    const description = document.createElement('p');
-
-    description.textContent = "Here at Optimistic's Fried Chicken, we are honored to serve you with top quality home-made fried chicken. We offer fried chicken, sides, desserts, and drinks, all made with culinary excellence. Our establishment is renowned for top tier customer service."
-
-    pageContent.appendChild(description);
-
-    const hours = document.createElement('p');
-
-    hours.innerHTML = "Sunday: 10am - 6pm <br> Monday: 6am - 10pm <br> Tuesday: 6am - 10pm <br> Wednesday: 6am - 10pm <br> Thursday: 6am - 10pm <br> Friday: 6am - 10pm <br> Saturday: 8am - 8pm <br>";
-
-    pageContent.appendChild(hours);
-
-    const location = document.createElement('p');
-
-    location.innerHTML = "We are located at: <br> 123 Millcreek Drive, Secaucus, New Jersey"
-
-    pageContent.appendChild(location);
-
+    renderHomePage();
 
 })
 
@@ -162,66 +141,6 @@ contactUsButton.addEventListener('click', () => {
 
     removePage();
     changePageTitle("Contact Us");
-
-    const employees = [
-        {
-            name: "Bob Joness",
-            position: "Manager",
-            phoneNumber: "123-456-7890",
-            email: "bobjonesss@gmail.com",
-            img: '../src/optimistictrousers.jpg',
-        },
-        {
-            name: "Christina Phillips",
-            position: "Cook",
-            phoneNumber: "124-164-4351",
-            email: "christinaphillips@gmail.com",
-            img: '../src/optimistictrousers.jpg',
-        },
-        {
-            name: "Loco Poco",
-            position: "Cashier",
-            phoneNumber: "121-321-4351",
-            email: "locopoco@gmail.com",
-            img: '../src/optimistictrousers.jpg',
-        },
-    ]
-
-    const contactInfo = document.createElement('div');
-
-    const paragraph = document.createElement('p');
-
-    const divContent = document.querySelector('div#content');
-
-    contactInfo.style = "display: grid; grid-template-columns: repeat(2, 1fr); grid-template-rows: repeat(2, 1fr); gap: 32px;"
-
-    paragraph.textContent = "Got a question about us or our clothing? Are you interested in partnering with us? Have some suggestions or just want to say Hi? Just contact us. We are here to asset you."
-
-    contactInfo.classList.add('contact-info');
-
-    employees.forEach(function (employee) {
-
-        const personInfo = document.createElement('div');
-
-        personInfo.style = "display: flex; flex-direction: column; align-items: center; gap: 16px; margin-top: 16px; padding: 1rem; border: 3px solid #f7f6f3;"
-
-        personInfo.innerHTML = "Name: " + employee.name + "<br> Position: " + employee.position + "<br> Phone Number: " + employee.email;
-
-        const img = document.createElement('img');
-
-        img.src = employee.img;
-
-        img.width = "100";
-
-        img.height = "100";
-
-        personInfo.appendChild(img);
-
-        contactInfo.appendChild(personInfo);
-
-    })
-
-
-    pageContent.appendChild(paragraph);
-    pageContent.appendChild(contactInfo);
+    renderHomePage();
+    
 })
---
+++ src/pages/contact-us.js
@@ -0,0 +1,64 @@
+function renderContactPage(){
+
+    const employees = [
+        {
+            name: "Bob Joness",
+            position: "Manager",
+            phoneNumber: "123-456-7890",
+            email: "bobjonesss@gmail.com",
+            img: '../src/optimistictrousers.jpg',
+        },
+        {
+            name: "Christina Phillips",
+            position: "Cook",
+            phoneNumber: "124-164-4351",
+            email: "christinaphillips@gmail.com",
+            img: '../src/optimistictrousers.jpg',
+        },
+        {
+            name: "Loco Poco",
+            position: "Cashier",
+            phoneNumber: "121-321-4351",
+            email: "locopoco@gmail.com",
+            img: '../src/optimistictrousers.jpg',
+        },
+    ]
+
+    const contactInfo = document.createElement('div');
+
+    const paragraph = document.createElement('p');
+
+    const divContent = document.querySelector('div#content');
+
+    contactInfo.style = "display: grid; grid-template-columns: repeat(2, 1fr); grid-template-rows: repeat(2, 1fr); gap: 32px;"
+
+    paragraph.textContent = "Got a question about us or our clothing? Are you interested in partnering with us? Have some suggestions or just want to say Hi? Just contact us. We are here to asset you."
+
+    contactInfo.classList.add('contact-info');
+
+    employees.forEach(function (employee) {
+
+        const personInfo = document.createElement('div');
+
+        personInfo.style = "display: flex; flex-direction: column; align-items: center; gap: 16px; margin-top: 16px; padding: 1rem; border: 3px solid #f7f6f3;"
+
+        personInfo.innerHTML = "Name: " + employee.name + "<br> Position: " + employee.position + "<br> Phone Number: " + employee.email;
+
+        const img = document.createElement('img');
+
+        img.src = employee.img;
+
+        img.width = "100";
+
+        img.height = "100";
+
+        personInfo.appendChild(img);
+
+        contactInfo.appendChild(personInfo);
+
+    })
+
+
+    pageContent.appendChild(paragraph);
+    pageContent.appendChild(contactInfo);
+}

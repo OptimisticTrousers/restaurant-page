107a08e43d64c6274e2d0dca3f9b9352958c71ee
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-20 13:53:56 -0400

    Reversing webpack.config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: ['./src/index.js', './src/page-load.js'],
@@ -7,12 +6,6 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-        plugins: [
-        new HtmlWebpackPlugin({
-          filename: 'index.html',
-          template: './src/index.html'  
-        })
-    ],
     module: {
         rules: [
             {

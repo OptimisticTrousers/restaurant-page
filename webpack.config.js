004fa1847fa158989f8ebabf5241c519258ceca9
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-20 13:47:26 -0400

    Adding plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: ['./src/index.js', './src/page-load.js'],
@@ -6,6 +7,12 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+        plugins: [
+        new HtmlWebpackPlugin({
+          filename: 'index.html',
+          template: './src/index.html'  
+        })
+    ],
     module: {
         rules: [
             {

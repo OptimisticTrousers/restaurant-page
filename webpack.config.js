cd9748fa4e111747a37dd3d3e395ea9656b0cd87
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-22 17:56:24 -0400

    Adding mode to production in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: "production",
     entry: ['./src/index.js', './src/page-load.js', './src/pages/home.js', './src/pages/menu.js', './src/pages/contact-us.js'],
     output: {
         filename: 'main.js',

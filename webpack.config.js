76582f955e837f08d13f15daa2f52809b19650fd
OptimisticTrousers <tony.isern@yahoo.com>
2022-06-22 17:44:57 -0400

    Changing webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: ['./src/index.js', './src/page-load.js'],
+    entry: ['./src/index.js', './src/page-load.js', './src/pages/home.js', './src/pages/menu.js', './src/pages/contact-us.js'],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
